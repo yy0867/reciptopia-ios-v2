b32fc2dfda66a9986e358badbcc4c6245f44f5f0
김세영 <yy0867@gmail.com>
2022-03-30 00:59:22 +0900

    프로젝트에 커스텀 모듈 추가
    Reciptopia_iOS는 Presentation Layer을 담당하는 모듈
    Reciptopia_UIKit은 UIKit의 확장이며, 재사용 뷰 등을 담고 있는 모듈

---
+++ ReciptopiaUIKit/ReciptopiaUIKit.h
@@ -0,0 +1,18 @@
+//
+//  ReciptopiaUIKit.h
+//  ReciptopiaUIKit
+//
+//  Created by 김세영 on 2022/03/29.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ReciptopiaUIKit.
+FOUNDATION_EXPORT double ReciptopiaUIKitVersionNumber;
+
+//! Project version string for ReciptopiaUIKit.
+FOUNDATION_EXPORT const unsigned char ReciptopiaUIKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ReciptopiaUIKit/PublicHeader.h>
+
+
---
+++ Reciptopia_iOS/Reciptopia_iOS.h
@@ -0,0 +1,18 @@
+//
+//  Reciptopia_iOS.h
+//  Reciptopia_iOS
+//
+//  Created by 김세영 on 2022/03/29.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Reciptopia_iOS.
+FOUNDATION_EXPORT double Reciptopia_iOSVersionNumber;
+
+//! Project version string for Reciptopia_iOS.
+FOUNDATION_EXPORT const unsigned char Reciptopia_iOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Reciptopia_iOS/PublicHeader.h>
+
+
