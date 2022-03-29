68fa86648c192c8d61657b128b49e0b5feb4e130
김세영 <yy0867@gmail.com>
2022-03-30 00:56:46 +0900

    프로젝트 생성

---
+++ Reciptopia/AppDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.swift
+//  Reciptopia
+//
+//  Created by 김세영 on 2022/03/29.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
+
