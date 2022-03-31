d01fbc241f0d4343b2ef65d2ff615ee5807d3ddf
김세영 <yy0867@gmail.com>
2022-03-31 23:04:25 +0900

    PictureIngredientViewModel의 action 바인딩
    바인딩 후 호출할 메서드까지만 구현

--- Reciptopia_iOS/PictureIngredient/PictureIngredientViewController.swift
+++ Reciptopia_iOS/PictureIngredient/PictureIngredientViewController.swift
@@ -6,11 +6,14 @@
 //
 
 import ReciptopiaUIKit
+import RxSwift
+import RxRelay
 
 public final class PictureIngredientViewController: BaseViewController {
     
     // MARK: - Properties
     let viewModel: PictureIngredientViewModel
+    let disposeBag = DisposeBag()
     
     lazy var searchButton: UIButton = {
         let button = UIButton()
@@ -37,6 +40,7 @@ public final class PictureIngredientViewController: BaseViewController {
     public init(viewModel: PictureIngredientViewModel) {
         self.viewModel = viewModel
         super.init()
+        observeViewModel()
     }
     
     public override func viewDidLoad() {
@@ -57,5 +61,42 @@ public final class PictureIngredientViewController: BaseViewController {
             action: #selector(PictureIngredientViewModel.searchByName),
             for: .touchUpInside
         )
+    }
+}
+
+// MARK: - Bind ViewModel
+extension PictureIngredientViewController {
+    func observeViewModel() {
+        viewModel.action
+            .bind(onNext: { [weak self] action in
+                switch action {
+                    case .search: self?.presentSearch()
+                    case .notification: self?.presentNotification()
+                    case .managePicture: self?.presentManagePicture()
+                    case .analyze: self?.presentAnalyze()
+                    case .album: self?.presentAlbum()
+                }
+            })
+            .disposed(by: disposeBag)
+    }
+    
+    private func presentSearch() {
+        
+    }
+    
+    private func presentNotification() {
+        
+    }
+    
+    private func presentManagePicture() {
+        
+    }
+    
+    private func presentAnalyze() {
+        
+    }
+    
+    private func presentAlbum() {
+        
     }
 }
