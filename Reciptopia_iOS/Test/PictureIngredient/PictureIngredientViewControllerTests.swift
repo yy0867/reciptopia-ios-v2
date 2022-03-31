edcb88c972726dff821b3e358b2b74481096a113
김세영 <yy0867@gmail.com>
2022-03-31 12:38:10 +0900

    PictureIngredientViewController 테스트 추가
    - PictureIngredientNavigationController가 rootVC로 PictureIngredientViewController을 가지고 있는지 체크
    - titleItem이 UIButton인지 체크
    - titleItem의 이미지가 "magnifyingglass", 타이틀이 "이름으로 검색" 인지 체크
    - rightBarButtonItem의 이미지가 "bell.fill" 인지 체크

---
+++ Reciptopia_iOS/Test/PictureIngredient/PictureIngredientViewControllerTests.swift
@@ -0,0 +1,61 @@
+//
+//  PictureIngredientViewControllerTests.swift
+//  Reciptopia_iOS
+//
+//  Created by 김세영 on 2022/03/31.
+//
+
+import XCTest
+@testable import Reciptopia_iOS
+
+class PictureIngredientViewControllerTests: XCTestCase {
+    
+    var pictureIngredientNavigationController: PictureIngredientNavigationController!
+    var viewModel: PictureIngredientViewModel!
+    var pictureIngredientViewController: PictureIngredientViewController!
+    
+    override func setUp() {
+        viewModel = PictureIngredientViewModel()
+        pictureIngredientNavigationController = PictureIngredientNavigationController(
+            viewModel: viewModel
+        )
+        guard let pictureIngredientViewController =
+                pictureIngredientNavigationController.topViewController
+                as? PictureIngredientViewController else {
+            XCTFail("\(#function) - get view controller from navigation controller fails.")
+            return
+        }
+        self.pictureIngredientViewController = pictureIngredientViewController
+        self.pictureIngredientViewController.viewDidLoad()
+    }
+    
+    func test_PictureIngredientViewController_titleItem_shouldBeSearchButton() {
+        // Given
+        guard let titleItem = pictureIngredientViewController.navigationItem.titleView as? UIButton else {
+            XCTFail("title item is not a UIButton.")
+            return
+        }
+        
+        // When
+        let title = titleItem.title(for: .normal)
+        let image = titleItem.image(for: .normal)
+        
+        // Then
+        XCTAssertEqual(title, "이름으로 검색")
+        XCTAssertEqual(UIImage(systemName: "magnifyingglass"), image)
+    }
+    
+    func test_PictureIngredientViewController_rightBarButtonItem_shouldBeNotificationButton() {
+        // Given
+        guard let rightItem = pictureIngredientViewController.navigationItem.rightBarButtonItem else {
+            XCTFail("right bar button item is nil.")
+            return
+        }
+        
+        // When
+        let image = rightItem.image
+        
+        // Then
+        XCTAssertEqual(UIImage(systemName: "bell.fill"), image)
+    }
+}
