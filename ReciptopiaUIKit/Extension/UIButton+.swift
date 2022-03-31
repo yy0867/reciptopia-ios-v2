c287f114e02e3c067be465cbdbbf98dc5952f499
김세영 <yy0867@gmail.com>
2022-03-31 23:01:10 +0900

    setBackgroundColor(_:for:) 편의 메서드 구현
    state별 background을 설정하는 메서드
    UIGraphicsImageContext()와 setBackgroundImage(_:for:)을 통해 구현

--- ReciptopiaUIKit/Extension/UIButton+.swift
+++ ReciptopiaUIKit/Extension/UIButton+.swift
@@ -7,8 +7,9 @@
 
 import UIKit
 
-// MARK: - Background and Title
 public extension UIButton {
+    
+    // MARK: - Properties
     
     // MARK: - Methods
     convenience init(
@@ -22,5 +23,18 @@ public extension UIButton {
         self.setTitleColor(titleColor, for: .normal)
         self.setTitle(title, for: .normal)
         self.titleLabel?.font = font
+        self.layer.masksToBounds = true
+    }
+    
+    func setBackgroundColor(_ color: UIColor, for state: UIControl.State) {
+        UIGraphicsBeginImageContext(CGSize(width: 1.0, height: 1.0))
+        guard let context = UIGraphicsGetCurrentContext() else { return }
+        context.setFillColor(color.cgColor)
+        context.fill(CGRect(x: 0.0, y: 0.0, width: 1.0, height: 1.0))
+        
+        let backgroundImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        
+        self.setBackgroundImage(backgroundImage, for: state)
     }
 }
