1b66ecbfd774d5d1b3f81589acce43f3335fc7c3
김세영 <yy0867@gmail.com>
2022-03-31 12:20:36 +0900

    RxRelay의 BehaviorRelay에 remove(at:) 메서드 추가
    기존의 삭제는 두 단계 이상 (삭제 후 방출) 의 과정을 거쳐야 했는데
    이를 메서드 하나로 통합하여 편리하게 사용 가능

--- Reciptopia_iOS/Extension/RxRelayExtension/BehaviorRelay+.swift
+++ Reciptopia_iOS/Extension/RxRelayExtension/BehaviorRelay+.swift
@@ -12,4 +12,10 @@ public extension BehaviorRelay {
     func append<T>(_ element: T) where Element == [T] {
         self.accept(self.value + [element])
     }
+    
+    func remove<T>(at index: Int) where Element == [T] {
+        var mutableValue = self.value
+        mutableValue.remove(at: index)
+        self.accept(mutableValue)
+    }
 }
