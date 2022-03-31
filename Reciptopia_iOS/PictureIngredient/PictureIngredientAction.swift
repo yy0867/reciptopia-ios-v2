727a38c3df078b7a5511fd8e4599bc9b78962409
김세영 <yy0867@gmail.com>
2022-03-31 12:22:45 +0900

    PictureIngredientView의 상태를 선언

---
+++ Reciptopia_iOS/PictureIngredient/PictureIngredientAction.swift
@@ -0,0 +1,18 @@
+//
+//  PictureIngredientAction.swift
+//  Reciptopia_iOS
+//
+//  Created by 김세영 on 2022/03/30.
+//
+
+enum PictureIngredientAction {
+    // MARK: Root View
+    case album
+    case managePicture
+    case analyze
+    
+    // MARK: Navigation Bar
+    case search
+    case notification
+    
+}
