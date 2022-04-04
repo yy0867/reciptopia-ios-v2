35645a6b63a56752c786755c1297540838290f66
김세영 <yy0867@gmail.com>
2022-04-04 12:54:17 +0900

    ReciptopiaKit 모듈 추가

---
+++ ReciptopiaKit/ReciptopiaKit.h
@@ -0,0 +1,18 @@
+//
+//  ReciptopiaKit.h
+//  ReciptopiaKit
+//
+//  Created by 김세영 on 2022/04/01.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ReciptopiaKit.
+FOUNDATION_EXPORT double ReciptopiaKitVersionNumber;
+
+//! Project version string for ReciptopiaKit.
+FOUNDATION_EXPORT const unsigned char ReciptopiaKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ReciptopiaKit/PublicHeader.h>
+
+
