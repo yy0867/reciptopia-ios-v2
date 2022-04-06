eb575397018805091284b2b58f7f14b9d7ad2717
김세영 <yy0867@gmail.com>
2022-04-06 15:31:31 +0900

    SearchIngredientRootView 테스트 작성
    - viewModel.addIngredient(name:)으로 추가한 재료의 개수와 collectionView의 cell의 개수가 같은지 체크
    - rootView의 remove(at:)을 호출했을 때 제대로 삭제되었는지 체크
    - ~~rootView의 changeState(to:at:)을 호출했을 때 cell의 backgroundColor이 변경되었는지 체크(보류)~~

---
+++ Reciptopia_iOS/Test/SearchIngredient/SearchIngredientRootViewTests.swift
@@ -0,0 +1,83 @@
+//
+//  SearchIngredientRootViewTests.swift
+//  Reciptopia_iOSTests
+//
+//  Created by 김세영 on 2022/04/06.
+//
+
+import XCTest
+import RxSwift
+@testable import Reciptopia_iOS
+@testable import ReciptopiaUIKit
+
+class SearchIngredientRootViewTests: XCTestCase {
+    
+    var viewModel: SearchIngredientViewModel!
+    var rootView: SearchIngredientRootView!
+    let disposeBag = DisposeBag()
+    
+    override func setUp() {
+        viewModel = SearchIngredientViewModel()
+        rootView = SearchIngredientRootView(viewModel: viewModel)
+    }
+    
+    func test_SearchIngredientRootView_addIngredient_shouldAddCollectionViewCell() {
+        // Given
+        let randomCount = Int.random(in: 10...50)
+        
+        // When
+        for _ in 0..<randomCount { viewModel.addIngredient(name: randomString()) }
+        
+        // Then
+        let collectionViewCellCount = rootView.ingredientCollectionView.numberOfItems(inSection: 0)
+        XCTAssertEqual(randomCount, collectionViewCellCount)
+    }
+    
+    func test_SearchIngredientRootView_removeIngredient_shouldRemoveCollectionViewCell() {
+        // Given
+        let randomCount = Int.random(in: 10...50)
+        var receivedIngredientNames = [String]()
+        var ingredientNameSource = [String]()
+        for _ in 0..<randomCount {
+            let name = randomString()
+            viewModel.addIngredient(name: name)
+            ingredientNameSource.append(name)
+        }
+        
+        // When
+        viewModel.ingredients
+            .map { $0.map { $0.name } }
+            .bind(onNext: { ingredientNames in
+                receivedIngredientNames = ingredientNames
+            })
+            .disposed(by: disposeBag)
+        
+        let randomAmount = Int.random(in: 1..<randomCount)
+        for i in stride(from: randomAmount, to: 0, by: -1) {
+            rootView.remove(at: i)
+            ingredientNameSource.remove(at: i)
+        }
+        
+        // Then
+        XCTAssertEqual(receivedIngredientNames, ingredientNameSource)
+    }
+    
+    func test_SearchIngredientRootView_changeStateAtIndex_shouldChangeCellAtIndexColor() {
+        // Given
+        let randomCount = Int.random(in: 10...50)
+        for _ in 0..<randomCount { viewModel.addIngredient(name: randomString()) }
+        
+        // When
+        let index = Int.random(in: 0..<randomCount)
+        rootView.changeState(to: .mainIngredient, at: index)
+        
+        // Then
+        let indexPath = IndexPath(item: index, section: 0)
+        guard let cell = rootView.ingredientCollectionView.dequeue(IngredientCell.self, at: indexPath) else {
+            XCTFail()
+            return
+        }
+//        print(cell.backgroundColor)
+//        XCTAssertEqual(UIColor.mainIngredient, cell.backgroundColor)
+    }
+}
