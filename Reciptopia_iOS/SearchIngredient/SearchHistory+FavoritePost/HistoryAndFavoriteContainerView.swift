3e9c76d8c27de3e1e15c41fb779eb4b1908fcc44
김세영 <yy0867@gmail.com>
2022-04-06 23:32:26 +0900

    검색 기록과 즐겨찾기를 담고 있는 HistoryAndFavoriteContainerView 구현

---
+++ Reciptopia_iOS/SearchIngredient/SearchHistory+FavoritePost/HistoryAndFavoriteContainerView.swift
@@ -0,0 +1,30 @@
+//
+//  HistoryAndFavoriteContainerView.swift
+//  Reciptopia_iOS
+//
+//  Created by 김세영 on 2022/04/06.
+//
+
+import ReciptopiaUIKit
+
+final class HistoryAndFavoriteContainerView: ContainerView {
+    
+    // MARK: - Properties
+    lazy var searchHistoryView: SearchHistoryRootView = {
+        let viewModel = SearchHistoryViewModel()
+        let view = SearchHistoryRootView(viewModel: viewModel)
+        return view
+    }()
+    
+    lazy var favoritePostView: FavoritePostRootView = {
+        let view = FavoritePostRootView()
+        return view
+    }()
+    
+    // MARK: - Methods
+    init() {
+        super.init(frame: .zero)
+        setSubviewList([searchHistoryView, favoritePostView])
+        presentSubview(at: 0)
+    }
+}
