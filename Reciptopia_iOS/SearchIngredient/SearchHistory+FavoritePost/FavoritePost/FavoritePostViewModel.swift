a1c6508b37c04eeabf1e6d9355b55b535595cc03
김세영 <yy0867@gmail.com>
2022-04-07 17:51:44 +0900

    FavoritePostViewModel 구현

--- Reciptopia_iOS/SearchIngredient/SearchHistory+FavoritePost/FavoritePost/FavoritePostViewModel.swift
+++ Reciptopia_iOS/SearchIngredient/SearchHistory+FavoritePost/FavoritePost/FavoritePostViewModel.swift
@@ -6,7 +6,34 @@
 //
 
 import Foundation
+import RxRelay
+import ReciptopiaKit
 
-final class FavoritePostViewModel {
+protocol FavoritePostViewModelInput {
     
+}
+
+protocol FavoritePostViewModelOutput {
+    var favoritePosts: BehaviorRelay<[Favorite]> { get }
+}
+
+protocol FavoritePostViewModelProtocol:
+    FavoritePostViewModelInput,
+    FavoritePostViewModelOutput {}
+
+final class FavoritePostViewModel: FavoritePostViewModelProtocol {
+    
+    // MARK: - Properties
+    let favoritePosts = BehaviorRelay<[Favorite]>(value: [])
+    
+    // MARK: - Methods
+    init() {
+        let favorite1 = Favorite(postId: 0, postName: "Favorite1")
+        let favorite2 = Favorite(postId: 0, postName: "Favorite2")
+        let favorite3 = Favorite(postId: 0, postName: "Favorite3")
+        let favorite4 = Favorite(postId: 0, postName: "Favorite4")
+        let favorites = [favorite1, favorite2, favorite3, favorite4]
+        
+        favoritePosts.accept(favorites)
+    }
 }
