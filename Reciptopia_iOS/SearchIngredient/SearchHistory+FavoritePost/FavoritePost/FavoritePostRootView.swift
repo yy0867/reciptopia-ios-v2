243c7cfb5136adc6128eb9b9aa0d939afaf7dda5
김세영 <yy0867@gmail.com>
2022-04-07 16:39:18 +0900

    FavoritePost 프레젠터 추가

---
+++ Reciptopia_iOS/SearchIngredient/SearchHistory+FavoritePost/FavoritePost/FavoritePostRootView.swift
@@ -0,0 +1,17 @@
+//
+//  FavoritePostRootView.swift
+//  Reciptopia_iOS
+//
+//  Created by 김세영 on 2022/04/06.
+//
+
+import ReciptopiaUIKit
+
+final class FavoritePostRootView: BaseView {
+    
+    // MARK: - Properties
+    
+    
+    // MARK: - Methods
+    
+}
---
+++ Reciptopia_iOS/SearchIngredient/SearchHistory+FavoritePost/FavoritePost/FavoritePostViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  FavoritePostViewModel.swift
+//  Reciptopia_iOS
+//
+//  Created by 김세영 on 2022/04/05.
+//
+
+import Foundation
+
+final class FavoritePostViewModel {
+    
+}
