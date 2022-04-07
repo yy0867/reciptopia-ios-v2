6ce46becd543754de3ed8819b7d59ed23294037f
김세영 <yy0867@gmail.com>
2022-04-07 16:39:35 +0900

    PostSearchListView 추가

---
+++ Reciptopia_iOS/SearchIngredient/PostSearchResult/PostSearchListView.swift
@@ -0,0 +1,20 @@
+//
+//  PostSearchListView.swift
+//  Reciptopia_iOS
+//
+//  Created by 김세영 on 2022/04/06.
+//
+
+import ReciptopiaUIKit
+
+final class PostSearchListView: BaseView {
+    
+    // MARK: - Properties
+    
+    
+    // MARK: - Methods
+    init() {
+        super.init(frame: .zero)
+        self.backgroundColor = .blue
+    }
+}
