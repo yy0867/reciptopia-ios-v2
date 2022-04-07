8648d847a02acbd05c4b12947ad638bff5a5ab9b
김세영 <yy0867@gmail.com>
2022-04-07 16:39:01 +0900

    뷰에 검색을 위한 FloatingButton 추가

--- Reciptopia_iOS/SearchIngredient/SearchHistory+FavoritePost/HistoryAndFavoriteRootView.swift
+++ Reciptopia_iOS/SearchIngredient/SearchHistory+FavoritePost/HistoryAndFavoriteRootView.swift
@@ -23,16 +23,27 @@ final class HistoryAndFavoriteRootView: BaseView {
         return containerView
     }()
     
+    lazy var searchFloatingButton: FloatingButton = {
+        let button = FloatingButton(size: 50)
+        button.setImage(UIImage(systemName: "magnifyingglass"))
+        return button
+    }()
+    
     // MARK: - Methods
     override func buildHierarchy() {
         addSubview(pageSegment)
         addSubview(historyAndFavoriteContainerView)
+        addSubview(searchFloatingButton)
     }
     
     override func activateConstraints() {
         pageSegment.snp.makeConstraints { make in
             make.top.leading.trailing.equalToSuperview()
             make.height.equalTo(45)
+        }
+        searchFloatingButton.snp.makeConstraints { make in
+            make.bottom.equalTo(safeAreaLayoutGuide).inset(30)
+            make.trailing.equalTo(safeAreaLayoutGuide).inset(20)
         }
         historyAndFavoriteContainerView.snp.makeConstraints { make in
             make.top.equalTo(pageSegment.snp.bottom)
