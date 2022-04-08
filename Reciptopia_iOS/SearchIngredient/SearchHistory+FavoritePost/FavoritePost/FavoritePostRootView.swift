60cdda61fd9c465ebcd7e54adfdf2bdfba9f6b90
김세영 <yy0867@gmail.com>
2022-04-08 12:58:07 +0900

    아직 사용하지 않는 didSelectRowAt 메서드 제거

--- Reciptopia_iOS/SearchIngredient/SearchHistory+FavoritePost/FavoritePost/FavoritePostRootView.swift
+++ Reciptopia_iOS/SearchIngredient/SearchHistory+FavoritePost/FavoritePost/FavoritePostRootView.swift
@@ -54,11 +54,7 @@ extension FavoritePostRootView {
 }
 
 // MARK: - UITableView Delegate
-extension FavoritePostRootView: UITableViewDelegate {
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        
-    }
-    
+extension FavoritePostRootView: UITableViewDelegate {    
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return 70
     }
