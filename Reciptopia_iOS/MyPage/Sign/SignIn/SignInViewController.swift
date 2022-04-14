2bce07c56f59054153235ace73dee6c73b72e837
김세영 <yy0867@gmail.com>
2022-04-14 16:03:10 +0900

    fix: ViewModel을 ViewModelProtocol로 변경 add: viewModel에서 방출하는 SignInAction을 구독 및 행동 처리

--- Reciptopia_iOS/MyPage/Sign/SignIn/SignInViewController.swift
+++ Reciptopia_iOS/MyPage/Sign/SignIn/SignInViewController.swift
@@ -6,20 +6,41 @@
 //
 
 import ReciptopiaUIKit
+import RxSwift
+import RxCocoa
 
 public final class SignInViewController: BaseViewController {
     
     // MARK: - Properties
-    let viewModel: SignInViewModel
+    let viewModel: SignInViewModelProtocol
+    let disposeBag = DisposeBag()
     
     // MARK: - Methods
-    public init(viewModel: SignInViewModel) {
+    public init(viewModel: SignInViewModelProtocol) {
         self.viewModel = viewModel
         super.init()
+        observeViewModel()
     }
     
     public override func viewDidLoad() {
         super.viewDidLoad()
         view = SignInRootView(viewModel: viewModel)
+    }
+    
+    private func observeViewModel() {
+        viewModel.action
+            .bind(onNext: { [weak self] action in
+                switch action {
+                    case .signUp: self?.presentSignUp()
+                }
+            })
+            .disposed(by: disposeBag)
+    }
+    
+    private func presentSignUp() {
+        let vm = SignUpViewModel()
+        let signUpViewController = SignUpViewController(viewModel: vm)
+        signUpViewController.title = "회원가입"
+        navigationController?.pushViewController(signUpViewController, animated: true)
     }
 }
