bfe68f8878672efcf5f99873d92fe66fe6439db9
김세영 <yy0867@gmail.com>
2022-04-28 14:54:24 +0900

    add: registerNib(_:) 메서드 추가
    nib으로 구성된 cell을 등록할 수 있는 메서드

--- ReciptopiaUIKit/Extension/UITableView+.swift
+++ ReciptopiaUIKit/Extension/UITableView+.swift
@@ -14,6 +14,12 @@ public extension UITableView {
         register(cellClass.self, forCellReuseIdentifier: reuseIdentifier)
     }
     
+    func registerNib(_ nibCellClass : UITableViewCell.Type) {
+        let reuseIdentifier = nibCellClass.reuseIdentifier
+        let nib = UINib(nibName: reuseIdentifier, bundle: Bundle(for: nibCellClass.self))
+        register(nib, forCellReuseIdentifier: reuseIdentifier)
+    }
+    
     func dequeue<T>(_ CellClass: T.Type, at indexPath: IndexPath) -> T? where T: UITableViewCell {
         guard let cell = self.dequeueReusableCell(
             withIdentifier: T.reuseIdentifier,
